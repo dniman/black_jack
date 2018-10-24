90655196834ef73a78d779cbfa12fcc345e31b87
dniman <dade.niman@gmail.com>
2018-10-25 02:10:43 +0300

    rubocop

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,22 +1,28 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
-require 'forwardable'
-
-class Deck
-  extend Forwardable
-
-  def_delegators :@cards, :shuffle, :<<
-
-  def initialize
-    @cards = []
-  end
-end
 
 describe Deck do
-  describe "respond to #shuffle" do
+  describe '#shuffle' do
     it { is_expected.to respond_to(:shuffle) }
+
+    it 'should be an instance of Deck' do
+      shuffled_subject = subject.shuffle.class
+      expect(shuffled_subject).to eq(subject.class)
+    end
   end
-  
-  describe "should respond to #<<" do
+
+  describe 'should respond to #<<' do
     it { is_expected.to respond_to(:<<) }
+  end
+
+  describe '#length' do
+    it 'should respond to #length' do
+      expect(subject).to respond_to(:length)
+    end
+
+    it 'should return 52 cards' do
+      expect(subject.length).to eq(52)
+    end
   end
 end
